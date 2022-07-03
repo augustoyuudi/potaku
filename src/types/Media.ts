8590430fb014b0e6939f04359ba005cef3829148
augustoyuudi <augustoyuudi@gmail.com>
2022-07-03 15:28:44 -0300

    feat: update types after removing query fields

--- src/types/Media.ts
+++ src/types/Media.ts
@@ -2,11 +2,8 @@ export default interface Media {
   id: number
   title: {
     romaji: string
-    native: string
   }
   coverImage: {
-    medium: string
     large: string
-    extraLarge: string
   }
 }
