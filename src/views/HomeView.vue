0b9f3d99099a3ad85f67c5f5407080f33116b5ed
augustoyuudi <augustoyuudi@gmail.com>
2022-07-07 21:31:22 -0300

    feat: style main page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,12 @@
 </script>
 
 <template>
-  <main>
-    <router-link to="va-quiz">who is the va?</router-link>
+  <main class="w-screen h-screen">
+    <h1 class="block uppercase my-8 text-center text-4xl">potaku</h1>
+    <ul class="block text-center">
+      <li>
+        <router-link to="va-quiz">Guess the voice actor</router-link>
+      </li>
+    </ul>
   </main>
 </template>
