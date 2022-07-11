bec0c9a4f58c83ba39a5a78cd334887ee6bbd1f6
augustoyuudi <augustoyuudi@gmail.com>
2022-07-11 13:09:42 -0300

    fix: wrap search bar and anime list

--- src/views/VAQuizView.vue
+++ src/views/VAQuizView.vue
@@ -97,7 +97,7 @@ function validateSelectedVA(va: number) {
 </script>
 
 <template>
-  <main class="flex items-center p-2">
+  <main class="flex flex-wrap items-center p-2">
     <form class="flex items-center gap-x-2 mb-4 b b-amber-400 rounded-md px-2 h-9 w-72" action="va-quiz" @submit.prevent="searchAnime">
       <label class="i-carbon-search text-purple-800 text-lg" for="searchAnime"></label>
       <input
