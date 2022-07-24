4f4c64a6e5b9f0b46b553b493cb388fc8fb19ffb
augustoyuudi <augustoyuudi@gmail.com>
2022-07-24 15:18:39 -0300

    feat: modularize home link style to unocss shortcut

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,7 +8,7 @@
       <li class="overflow-hidden">
         <router-link
           to="va-quiz"
-          class="text-pink-500 uppercase text-2xl font-bold relative after:content-none after:absolute after:left-0 after:bg-pink-100 after:w-full after:h-full after:-z-10 after:transition-transform after:duration-300 after:translate-x-1.5 after:translate-y-4 hover:after:translate-x-0 hover:after:translate-y-0"
+          class="home-link"
         >
             Guess the voice actor
           </router-link>
--- unocss.config.ts
+++ unocss.config.ts
@@ -14,5 +14,8 @@ export default defineConfig({
       }
     }),
     presetUno()
-  ]
+  ],
+  shortcuts: {
+    'home-link': 'text-pink-500 uppercase text-2xl font-bold relative after:content-none after:absolute after:left-0 after:bg-pink-100 after:w-full after:h-full after:-z-10 after:transition-transform after:duration-300 after:translate-x-1.5 after:translate-y-4 hover:after:translate-x-0 hover:after:translate-y-0'
+  }
 })
