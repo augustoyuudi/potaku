78da9e4db1893f060ad7a3f54b9bbf1ebb197df0
augustoyuudi <augustoyuudi@gmail.com>
2022-07-25 23:13:59 -0300

    fix: use isomorphic unfetch lib to find global fetch in tests

--- src/api/ApolloClient.ts
+++ src/api/ApolloClient.ts
@@ -1,9 +1,11 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client/core'
+import fetch from 'isomorphic-unfetch'
 
 // HTTP connection to the API
 const httpLink = createHttpLink({
   // You should use an absolute URL here
   uri: 'https://graphql.anilist.co/',
+  fetch
 })
 
 // Cache implementation
