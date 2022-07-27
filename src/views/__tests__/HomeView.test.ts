3302c67edc89270354b6fdd351905c298fac3259
augustoyuudi <augustoyuudi@gmail.com>
2022-07-27 17:58:34 -0300

    fix: remove unused test mocks from HomeView test

--- src/views/__tests__/HomeView.test.ts
+++ src/views/__tests__/HomeView.test.ts
@@ -1,13 +1,8 @@
 import { render, screen } from '@testing-library/vue'
-import { server } from '@/mocks/apollo'
 import router from '@/router'
 import sut from '../HomeView.vue'
 
 describe('HomeView', () => {
-  beforeAll(() => server.listen())
-  afterEach(() => server.resetHandlers())
-  afterAll(() => server.close())
-
   it('should render correctly', () => {
     render(sut, {
       global: {
