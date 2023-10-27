7b0c9d8d548d05eb31553b0a427cb489f4c9b85e
augustoyuudi <augustoyuudi@gmail.com>
2023-10-26 23:30:07 -0300

    feat: create getMediaById function in repository

--- packages/backend/core/repository/MediaRepository.ts
+++ packages/backend/core/repository/MediaRepository.ts
@@ -30,6 +30,42 @@ async function getPaginatedMedia(variables, db = UrqlClient) {
   return result.data;
 };
 
+async function getMediaById(variables, db = UrqlClient) {
+  const query = `
+    query getMediaById ($id: Int, $sort: [CharacterSort], $page: Int, $perPage: Int) {
+      Media (id: $id) {
+        id
+        characters (sort: $sort, page: $page, perPage: $perPage) {
+          edges {
+            node {
+              image {
+                large
+              }
+              name {
+                full
+              }
+            }
+            voiceActors (language: JAPANESE) {
+              id
+              name {
+                full
+              }
+              image {
+                large
+              }
+            }
+          }
+        }
+      }
+    }
+  `;
+
+  const result = await db.query(query, variables).toPromise();
+
+  return result.data;
+};
+
 export default {
-  getPaginatedMedia
+  getPaginatedMedia,
+  getMediaById
 };
