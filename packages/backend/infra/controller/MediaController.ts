968c6f2bd7929bea6241dc165123068c807ac31a
augustoyuudi <augustoyuudi@gmail.com>
2023-10-26 23:32:27 -0300

    feat: expose new endpoints in controller

--- packages/backend/infra/controller/MediaController.ts
+++ packages/backend/infra/controller/MediaController.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import getPaginatedMediaUC from 'core/usecase/getPaginatedMedia';
+import getMediaById from 'core/usecase/getMediaById';
+import getRandomizedMediaCharacter from 'core/usecase/getRandomizedMediaCharacter';
 
 const router = express.Router();
 
-router.get('/media', async (req, res) => {
+router.get('/media/search', async (req, res) => {
   if (!req.body) {
     req.body = {
       page: 1,
@@ -16,6 +18,37 @@ router.get('/media', async (req, res) => {
   const media = await getPaginatedMediaUC(req.body);
 
   res.send(media);
+});
+
+router.get('/media', async (req, res) => {
+  if (!req.body) {
+    req.body = {
+      id: 21,
+      page: 1,
+      perPage: 10,
+      type: 'ANIME'
+    };
+  }
+
+  const media = await getMediaById(req.body);
+
+  res.send(media);
+});
+
+router.get('/media/character/random', async (req, res) => {
+  if (!req.body) {
+    req.body = {
+      id: 21,
+      page: 1,
+      perPage: 10,
+      sort: ['ROLE'],
+      type: 'ANIME'
+    };
+  }
+
+  const character = await getRandomizedMediaCharacter(req.body);
+
+  res.send(character);
 });
 
 export default router;
