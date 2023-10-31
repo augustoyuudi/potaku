edefbad4df45bcd7180ac0fe02fc6c91f03a2215
augustoyuudi <augustoyuudi@gmail.com>
2023-10-30 22:54:29 -0300

    feat: call CharacterController in server

--- packages/backend/server.ts
+++ packages/backend/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import mediaController from './infra/controller/MediaController';
+import characterController from './infra/controller/CharacterController';
 
 const app = express();
 const port = 3000;
@@ -7,6 +8,7 @@ const port = 3000;
 app.use(express.json());
 
 app.use('/', mediaController);
+app.use('/', characterController);
 
 app.listen(port, () => {
   console.log(`App running on port http://localhost:${port}`);
