147d22b1e2f690ba8c4fe4457f9cd86cf2911f4f
augustoyuudi <augustoyuudi@gmail.com>
2023-11-02 22:54:29 -0300

    feat: instantiate GameController in server

--- packages/backend/server.ts
+++ packages/backend/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import mediaController from './infra/controller/MediaController';
 import characterController from './infra/controller/CharacterController';
+import gameController from './infra/controller/GameController';
 
 const app = express();
 const port = 3000;
@@ -9,6 +10,7 @@ app.use(express.json());
 
 app.use('/', mediaController);
 app.use('/', characterController);
+app.use('/', gameController);
 
 app.listen(port, () => {
   console.log(`App running on port http://localhost:${port}`);
