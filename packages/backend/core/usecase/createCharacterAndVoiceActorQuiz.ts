b59f53a34bb1407deeaf030035131459eaafd9c8
augustoyuudi <augustoyuudi@gmail.com>
2023-12-14 01:53:53 -0300

    feat: add voice actor to answer if not present

--- packages/backend/core/usecase/createCharacterAndVoiceActorQuiz.ts
+++ packages/backend/core/usecase/createCharacterAndVoiceActorQuiz.ts
@@ -6,14 +6,26 @@ export default async function createCharacterAndVoiceActorQuiz(query) {
     ...query,
     id: query.mediaId
   });
+
+  // remove sub voice actors
+  character.voiceActors.splice(1);
+
   const voiceActors = await getRandomizedVoiceActors({
     ...query,
     id: query.mediaId,
     size: 5
   });
 
+  const duplicatedVoiceActor = voiceActors.find(va => va.id === character.voiceActors[0].id);
+
+  // add VA if not in list, to ensure correct answer is in the list.
+  if (!duplicatedVoiceActor) {
+    const randomizedIndex = Math.floor(Math.random() * 5);
+    voiceActors[randomizedIndex] = character.voiceActors[0];
+  }
+
   return {
     character,
-    voice_actors: voiceActors
+    voiceActors
   }
 }
